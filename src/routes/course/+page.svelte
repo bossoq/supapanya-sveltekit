c1090ac41fa228edc23a25baa2b13db9cda171e4
Kittipos Wajakajornrit <boss.zz@me.com>
2024-10-24 22:58:18 +0700

    Add course page

---
+++ src/routes/course/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <title>คอร์สเรียน - สถาบันศุภปัญญาไอ.เค.</title>
+</svelte:head>
+
+<div class="flex flex-col items-center bg-white gap-6 h-screen">
+  <div class="container flex flex-col">
+    <h1 class="text-2xl md:text-4xl text-gray-800 font-bold py-2">คอร์สปิดเทอม 2563</h1>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <section>
+        <h2 class="text-2xl md:text-4xl text-gray-800 font-semibold py-2">คอร์สสอบเข้า ม.1</h2>
+        <img
+          src="https://ceoyctjctahurvmlupfh.supabase.in/storage/v1/object/public/supapanya-assets/course-images/Page Post 048-summer-Course63_3.jpg"
+          alt="m1 course"
+          class="aspect-[2/3] object-cover"
+        />
+      </section>
+      <section>
+        <h2 class="text-2xl md:text-4xl text-gray-800 font-semibold py-2">
+          คอร์สเสริมทักษะ (ประถม)
+        </h2>
+        <img
+          src="https://ceoyctjctahurvmlupfh.supabase.in/storage/v1/object/public/supapanya-assets/course-images/Page Post 049-summer-Course63_4.jpg"
+          alt="prathom course"
+          class="aspect-[2/3] object-cover"
+        />
+      </section>
+    </div>
+  </div>
+</div>
