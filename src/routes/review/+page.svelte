94ce463c3250cf55ba686ab29acb7aa485544c4b
Kittipos Wajakajornrit <boss.zz@me.com>
2024-10-24 23:04:08 +0700

    Add review page

---
+++ src/routes/review/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <title>รีวิว - สถาบันศุภปัญญาไอ.เค.</title>
+</svelte:head>
+
+<div class="flex flex-col items-center bg-white gap-6 h-screen">
+  <div class="container flex flex-col">
+    <h1 class="text-2xl md:text-4xl text-gray-800 font-bold py-2">รีวิวจากผู้ปกครอง</h1>
+    <h2 class="text-2xl md:text-4xl text-gray-800 font-semibold py-2">ขอขอบพระคุณคุณแม่น้องเนม</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <section>
+        <img
+          src="https://ceoyctjctahurvmlupfh.supabase.in/storage/v1/object/public/supapanya-assets/review-images/001-1.jpg"
+          alt="review 1"
+          class="object-cover"
+        />
+      </section>
+      <section>
+        <img
+          src="https://ceoyctjctahurvmlupfh.supabase.in/storage/v1/object/public/supapanya-assets/review-images/001-2.jpg"
+          alt="review 2"
+          class="object-cover"
+        />
+      </section>
+    </div>
+    <p class="text-2xl italic font-medium p-8">
+      “ทางสถาบันศุภปัญญา ไอ.เค. ขอขอบพระคุณคุณแม่น้องเนม (ด.ช.พีระวิชญ์ สังข์กลม) เป็นอย่างสูง
+      ที่ให้ความไว้วางใจในสถาบันฯ ในการสนับสนุน อบรมสั่งสอน ทั้งด้านวิชาการ และด้านสังคม
+      เพื่อให้น้องเนมเป็นทั้งคนเก่ง และคนดี ทั้งในครอบครัว โรงเรียน และประเทศ”
+    </p>
+  </div>
+</div>
