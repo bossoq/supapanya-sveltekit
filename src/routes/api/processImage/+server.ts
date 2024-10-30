81aefa07c3fb46f4095bee8284b484398ad19db9
kittipos.w <kittipos.w@kasikornline.com>
2024-10-30 22:41:29 +0700

    Move resize image function to server side to fix vidstack not loading

--- src/routes/api/processImage/+server.ts
+++ src/routes/api/processImage/+server.ts
@@ -1,5 +1,5 @@
+import sharp from 'sharp'
 import { json } from '@sveltejs/kit'
-import { resizeImage } from '$lib/utils'
 import type { RequestHandler } from './$types'
 
 export const POST: RequestHandler = async ({ request }) => {
@@ -18,4 +18,21 @@ export const POST: RequestHandler = async ({ request }) => {
     return json({ success: false, message: 'Error resizing image' }, { status: 500 })
   }
   return json({ success: true, image: resizedImage })
+}
+
+const resizeImage = async (data: Buffer, imageType = 'full'): Promise<string> => {
+  if (!data) {
+    throw new Error('Invalid image')
+  }
+  const image = sharp(data)
+  const metadata = await image.metadata()
+  if (metadata.width && metadata.height) {
+    const aspectRatio = metadata.width / metadata.height
+    const newWidth = Math.min(metadata.width, imageType === 'full' ? 2048 : 640)
+    const newHeight = newWidth / aspectRatio
+    const resizedImage = await image.resize(newWidth, newHeight).webp().toBuffer()
+    return `data:image/webp;base64,${resizedImage.toString('base64')}`
+  } else {
+    throw new Error('Invalid image')
+  }
 }
