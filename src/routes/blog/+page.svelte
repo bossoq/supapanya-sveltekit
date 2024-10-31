254b6e6802c70321bc07b7bc23b7ef735db250f3
kittipos.w <kittipos.w@kasikornline.com>
2024-10-31 15:37:15 +0700

    Add new, edit, delete button

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -10,8 +10,19 @@
   <title>บทความที่น่าสนใจ - สถาบันศุภปัญญาไอ.เค.</title>
 </svelte:head>
 
-<div class="flex flex-col items-center bg-white gap-6 h-[calc(100vh-10.2rem)]">
+<div class="flex flex-col items-center bg-white gap-6 h-[calc(100vh-10.2rem)] pt-4">
   <div class="container flex flex-col">
+    <section class="absolute bottom-20 right-24">
+      <button
+        class="bg-teal-400 text-white p-2 rounded-full hover:bg-teal-700 transition-all ease-in-out duration-300 hover:rotate-180"
+        aria-label="เขียนบทความใหม่"
+        on:click={() => (window.location.href = '/blog/new')}
+      >
+        <svg class="aspect-square w-6" fill="currentColor">
+          <use xlink:href="{remixiconUrl}#ri-add-line" />
+        </svg>
+      </button>
+    </section>
     <section class="grid grid-cols-1 md:grid-cols-3 gap-4">
       {#each props.blogs as blog}
         <a
@@ -19,12 +30,34 @@
           href="/blog/{blog.postLink}"
         >
           <section class="flex flex-col items-center gap-2">
-            <img
-              src={blog.postPicture}
-              class="object-cover aspect-[3/2] hover:scale-105 transition-all ease-in-out duration-300"
-              alt={blog.postTitle}
-              loading="lazy"
-            />
+            <figure class="relative">
+              <img
+                src={blog.postPicture}
+                class="object-cover aspect-[3/2] hover:scale-105 transition-all ease-in-out duration-300"
+                alt={blog.postTitle}
+                loading="lazy"
+              />
+              <div class="absolute top-0 right-0">
+                <button
+                  class="bg-gray-400 text-white p-2 hover:bg-gray-700 transition-all ease-in-out duration-300 opacity-50 hover:opacity-100"
+                  aria-label="แก้ไขบทความ"
+                  on:click={() => (window.location.href = `/blog/${blog.postLink}/edit`)}
+                >
+                  <svg class="aspect-square w-6" fill="currentColor">
+                    <use xlink:href="{remixiconUrl}#ri-pencil-line" />
+                  </svg>
+                </button>
+                <button
+                  class="bg-gray-400 text-white p-2 hover:bg-gray-700 transition-all ease-in-out duration-300 opacity-50 hover:opacity-100"
+                  aria-label="แก้ไขบทความ"
+                  on:click={() => (window.location.href = `/blog/${blog.postLink}/delete`)}
+                >
+                  <svg class="aspect-square w-6" fill="currentColor">
+                    <use xlink:href="{remixiconUrl}#ri-delete-bin-line" />
+                  </svg>
+                </button>
+              </div>
+            </figure>
             <section class="flex flex-row items-center gap-1 text-gray-500 text-sm">
               <span>
                 <svg class="aspect-square w-5" fill="currentColor">
