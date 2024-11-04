706812e950971c87b470bcaa6eece796dc2c6ea6
kittipos.w <kittipos.w@kasikornline.com>
2024-11-04 23:21:16 +0700

    Add studytips page

---
+++ src/routes/studytips/+page.server.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client'
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async () => {
+  const prisma = new PrismaClient()
+  const videos = await prisma.videoTable.findMany({
+    select: {
+      id: true,
+      name: true,
+      baseUrl: true,
+      fileType: true
+    },
+    orderBy: {
+      id: 'asc'
+    },
+    where: {
+      type: 'studytips',
+      allowAll: true
+    }
+  })
+  return {
+    props: {
+      videos: videos.map((video) => {
+        return {
+          id: Number(video.id),
+          title: video.name,
+          url: video.baseUrl,
+          fileType: video.fileType
+        }
+      })
+    }
+  }
+}
---
+++ src/routes/studytips/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import type { PageServerData } from './$types'
+  import PlayerPlaylist from '$lib/components/PlayerPlaylist.svelte'
+
+  export let data: PageServerData
+  let videoList = data.props.videos
+</script>
+
+<svelte:head>
+  <title>เกร็ดความรู้ - สถาบันศุภปัญญาไอ.เค.</title>
+</svelte:head>
+
+<div class="flex flex-col items-center bg-white gap-6 h-[calc(100vh-10.2rem)]">
+  <div class="container flex flex-col">
+    <h1 class="text-2xl md:text-4xl text-gray-800 font-bold py-2">เกร็ดความรู้</h1>
+    <PlayerPlaylist bind:videoList restart autoPlay />
+  </div>
+</div>
