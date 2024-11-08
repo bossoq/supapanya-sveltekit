d8b737595338836f88721840cd4f9c2e94f42fa0
kittipos.w <kittipos.w@kasikornline.com>
2024-11-08 11:16:01 +0700

    Change VOD to use cookies to check user id & bypass logic for admin

--- src/routes/vod/+page.server.ts
+++ src/routes/vod/+page.server.ts
@@ -1,11 +1,42 @@
+import jwt from 'jwt-simple'
 import { redirect } from '@sveltejs/kit'
 import { PrismaClient } from '@prisma/client'
+import { JWT_SECRET } from '$env/static/private'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ locals }) => {
-  const user = locals.user
+export const load: PageServerLoad = async ({ cookies }) => {
+  let user: UserInfo | undefined
+  const accessToken = cookies.get('accessToken')
+  if (!accessToken || accessToken === null) {
+    return redirect(301, '/login')
+  }
+  try {
+    user = jwt.decode(accessToken, JWT_SECRET, false, 'HS256') as UserInfo
+  } catch (e) {
+    return redirect(301, '/login')
+  }
   if (!user) {
     return redirect(301, '/login')
+  }
+  let allowFilter: { [k: string]: unknown }
+  if (user.meta.isAdmin) {
+    allowFilter = {
+      type: 'vod'
+    }
+  } else {
+    allowFilter = {
+      type: 'vod',
+      OR: [
+        {
+          allowAll: true
+        },
+        {
+          allowList: {
+            array_contains: user.id
+          }
+        }
+      ]
+    }
   }
   const prisma = new PrismaClient()
   const videos = await prisma.videoTable.findMany({
@@ -19,19 +50,7 @@ export const load: PageServerLoad = async ({ locals }) => {
     orderBy: {
       id: 'asc'
     },
-    where: {
-      type: 'vod',
-      OR: [
-        {
-          allowAll: true
-        },
-        {
-          allowList: {
-            array_contains: user.id
-          }
-        }
-      ]
-    }
+    where: allowFilter
   })
   return {
     user: user,
